2928791e19d3a6e2a0249b157c2f70224670ad4c
ramisasamin <ramisasamin@gmail.com>
2021-03-22 16:57:37 +0600

    Added Notice Panel

--- app/src/main/java/com/example/studentapp/Portal.java
+++ app/src/main/java/com/example/studentapp/Portal.java
@@ -13,18 +13,34 @@ import java.io.BufferedReader;
 
 public class Portal extends AppCompatActivity
 {
-    TextView btnresult;
-    ImageView btnresulti;
+    TextView btnresult, btnattendance, btnnotice;
+    ImageView btnresulti, btnattendancei, btnnoticei;
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.portal);
 
+        //result
         btnresult=(TextView) findViewById(R.id.result);
         btnresulti = (ImageView) findViewById(R.id.resulti);
         btnresulti.setImageResource(R.drawable.result);
 
+
+        //attendance
+        btnattendance=(TextView) findViewById(R.id.attendance);
+        btnattendancei = (ImageView) findViewById(R.id.attendacei);
+        btnattendancei.setImageResource(R.drawable.attendance);
+
+
+        //notice
+        btnnotice=(TextView) findViewById(R.id.notice);
+        btnnoticei = (ImageView) findViewById(R.id.noticei);
+        btnnoticei.setImageResource(R.drawable.notice);
+
+
+
+        //result
         btnresult.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -37,6 +53,43 @@ public class Portal extends AppCompatActivity
             @Override
             public void onClick(View view) {
                 Intent intent=new Intent(Portal.this,Result.class);
+                startActivity(intent);
+            }
+        });
+
+
+
+        //attendance
+        btnattendance.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(Portal.this,Attendance.class);
+                startActivity(intent);
+            }
+        });
+
+        btnattendancei.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(Portal.this,Attendance.class);
+                startActivity(intent);
+            }
+        });
+
+
+        //notice
+        btnnotice.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(Portal.this,Notice.class);
+                startActivity(intent);
+            }
+        });
+
+        btnnoticei.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent=new Intent(Portal.this,Notice.class);
                 startActivity(intent);
             }
         });
