6b86a4803c3dc7e4fe3d49857982afdb1f96913c
ramisasamin <ramisasamin@gmail.com>
2021-03-22 16:11:38 +0600

    Student App

--- app/src/main/java/com/example/studentapp/Result.java
+++ app/src/main/java/com/example/studentapp/Result.java
@@ -5,12 +5,44 @@ import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.anychart.AnyChart;
+import com.anychart.AnyChartView;
+import com.anychart.chart.common.dataentry.DataEntry;
+import com.anychart.chart.common.dataentry.ValueDataEntry;
+import com.anychart.charts.Pie;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class Result extends AppCompatActivity
 {
+
+    AnyChartView anyChartView;
+    String[] terms = {"First Term", "Second Term", "Third Term"};
+    int[] marks = {90, 80, 95};
+
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.result);
+
+        anyChartView = findViewById(R.id.any_chart_view);
+        setupPieChart();
+    }
+
+    public void setupPieChart()
+    {
+        Pie pie = AnyChart.pie();
+        List<DataEntry> dataEntries = new ArrayList<>();
+
+        for(int i=0; i < terms.length; i++)
+        {
+            dataEntries.add(new ValueDataEntry(terms[i], marks[i]));
+        }
+
+        pie.data(dataEntries);
+//        pie.title("terms");
+        anyChartView.setChart(pie);
     }
 }
